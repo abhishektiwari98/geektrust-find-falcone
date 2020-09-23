8b8d95e7722cc44f676810a9e4b05c13c4dab0dd
Daffolap-908 <abhishek.kumar@unthinkable.co>
2020-09-23 15:15:01 +0530

    created build named docs

---
+++ docs/precache-manifest.a56ccd88687592d27fb901dc09ac486d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e176089c6ec1e9bd73cb1c688ad6e564",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c060be58941da17c2010",
+    "url": "/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "d82533c18e26b76ec736",
+    "url": "/static/css/main.dc0c78c9.chunk.css"
+  },
+  {
+    "revision": "c060be58941da17c2010",
+    "url": "/static/js/2.8446ad69.chunk.js"
+  },
+  {
+    "revision": "d82533c18e26b76ec736",
+    "url": "/static/js/main.80a06bdd.chunk.js"
+  },
+  {
+    "revision": "59aecfb06818ec9d5425",
+    "url": "/static/js/runtime-main.c795c248.js"
+  }
+]);
